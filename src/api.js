3f31984819d0e2ce917a5436eed15ac8b182cf9f
Anatolii Egorov <Egorov.A.Vy@sberbank.ru>
2020-09-15 02:16:39 +0300

    endpoint change

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: `http://localhost:8080/api/adminPanel`,
+    baseURL: `http://127.0.0.1:8080/api/adminPanel`,
     responseType: "json"
 });
