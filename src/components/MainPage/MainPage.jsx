6e85d33db107bd90c7c16c18318266de6cc6ec97
Anatolii Egorov <Egorov.A.Vy@sberbank.ru>
2020-09-16 14:36:46 +0300

    delete console.warn

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -10,10 +10,8 @@ export default class MainPage extends Component {
             (async () => {
                 try {
                     const {data} = await API.get(`/${this.state.id}/`);
-                    console.warn(data, 'data')
                     const newDate = this.state.data
                     newDate.push({...data, endPoint: `/${this.state.id}/`})
-                    console.warn(newDate, 'newDate')
                     this.setState({data: newDate})
                 } catch (e) {
                     throw new Error(e)
@@ -28,13 +26,12 @@ export default class MainPage extends Component {
     }
 
     render() {
-        console.warn(this.state.data, 'this.state.data')
         return (
             <div className="block">
                 {/*  <h1 className="block__header">My React App!</h1>*/}
                 <input type='number' pattern='^[ 0-9]+$' value={this.state.id} onChange={this.handleChangeId}/>
-                {this.state.data.length > 0 && this.state.data.map(elem => <div>{Object.keys(elem).map(prop =>
-                    <div>
+                {this.state.data.length > 0 && this.state.data.map((elem,index) => <div key={index}>{Object.keys(elem).map(prop =>
+                    <div key={prop.id}>
                         {prop}:{elem[prop]}
                     </div>)}
                 </div>)}
